b71d690f949c4fe650b29d61729594f6a29e4ea7
chou xue <k1vinzh@outlook.com>
2021-06-02 22:40:46 +0800

    Strings

--- src/scanner.c
+++ src/scanner.c
@@ -220,7 +220,7 @@ Token scanToken()
 	case ',': return makeToken(TOKEN_COMMA);
 	case '.': return makeToken(TOKEN_DOT);
 	case '-': return makeToken(TOKEN_MINUS);
-	case '+': return makeToken(TOKEN_AND);
+	case '+': return makeToken(TOKEN_PLUS);
 	case '/': return makeToken(TOKEN_SLASH);
 	case '*': return makeToken(TOKEN_STAR);
 	case '!': return makeToken(match('=') ? TOKEN_BANG_EQUAL : TOKEN_BANG);
