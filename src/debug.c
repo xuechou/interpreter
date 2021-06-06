97b96a520b7b0bf34d9fa5273309cfdda271fcda
chou xue <k1vinzh@outlook.com>
2021-06-06 17:19:00 +0800

    local variables

--- src/debug.c
+++ src/debug.c
@@ -16,6 +16,15 @@ static int simpleInstruction(const char *name, int offset)
 	printf("%s\n", name);
 	return offset + 1;
 }
+
+static int byteInstruction(const char* name, Chunk* chunk, int offset)
+{
+	uint8_t slot = chunk->code[offset+1];
+	printf("%-16s %4d\n", name, slot);
+	return offset+2;
+
+}
+
 static int constantInstruction(const char *name, Chunk *chunk,
 							   int offset)
 {
@@ -51,6 +60,10 @@ int disassembleInstruction(Chunk *chunk, int offset)
 		return simpleInstruction("OP_FALSE", offset);
 	case OP_POP:
 		return simpleInstruction("OP_POP", offset);
+	case OP_GET_LOCAL:
+		return byteInstruction("OP_GET_LOCAL", chunk, offset);
+	case OP_SET_LOCAL:
+		return byteInstruction("OP_SET_LOCAL", chunk, offset);
 	case OP_GET_GLOBAL:
 		return constantInstruction("OP_GET_GLOBAL", chunk, offset);
 	case OP_DEFINE_GLOBAL:
